4b90a88b63508e756e5563869042aaec3572d00b
Tim Loden <tim.m.loden@gmail.com>
2019-05-15 15:05:04 -0700

    added vendor script tage commented out

--- inc/scripts-and-styles.php
+++ inc/scripts-and-styles.php
@@ -16,6 +16,10 @@ add_action( 'wp_enqueue_scripts', 'dynacornparts_scripts', 99 );
 
 function dynacornparts_scripts() {
 
+	// Vendor scripts
+	//wp_enqueue_script( 'dynacorn-vendor-scripts', get_template_directory_uri() . '/assets/js/vendor.min.js', array(), '20151215', true );
+
+	// Custom scripts
 	wp_enqueue_script( 'dynacornparts-custom-scripts', get_template_directory_uri() . '/assets/js/custom.min.js', array('customize-preview'), '20151215', true );
 
 }
