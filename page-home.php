6644424714398e82cc6ee935896c6738a1d6b908
Tim Loden <tim.m.loden@gmail.com>
2019-08-02 09:59:15 -0700

    more phpcs

--- page-home.php
+++ page-home.php
@@ -18,38 +18,38 @@ get_header();
 ?>
 <!--slider area start-->
 
-<?php if( have_rows('hero_slider') ): ?>
+<?php if(have_rows('hero_slider') ) : ?>
 
 <section class="slider_section slider_two mb-50">
     <div class="slider_area owl-carousel">
 
-		<?php while( have_rows('hero_slider') ): the_row();
+    <?php while( have_rows('hero_slider') ): the_row();
 
-			// vars
-			$sub_title = get_sub_field('sub_title');
-			$title = get_sub_field('title');
-			$button_text = get_sub_field('button_text');
-			$button_link = get_sub_field('button_link');
-			$background_image = get_sub_field('background_image');
-			$text_color = get_sub_field('text_color');
+        // vars
+        $sub_title = get_sub_field('sub_title');
+        $title = get_sub_field('title');
+        $button_text = get_sub_field('button_text');
+        $button_link = get_sub_field('button_link');
+        $background_image = get_sub_field('background_image');
+        $text_color = get_sub_field('text_color');
 
-			?>
+        ?>
 
-			<div class="single_slider d-flex align-items-center" data-bgimg="<?php echo $background_image['url']; ?>">
-				<div class="container">
-	               <div class="row">
-	                   <div class="col-12">
-	                       <div class="slider_content">
-	                            <h2 style="color: <?php echo esc_attr($text_color); ?>;"><?php echo esc_attr($sub_title); ?></h2>
-	                            <h1 style="color: <?php echo esc_attr($text_color); ?>;"><?php echo esc_attr($title); ?></h1>
-	                            <a class="button" href="<?php echo esc_url($button_link); ?>"><?php echo esc_attr($button_text); ?></a>
-	                        </div>
-	                   </div>
-	               </div>
-	           </div>
-			</div>
+            <div class="single_slider d-flex align-items-center" data-bgimg="<?php echo $background_image['url']; ?>">
+                <div class="container">
+                   <div class="row">
+                       <div class="col-12">
+                           <div class="slider_content">
+                                <h2 style="color: <?php echo esc_attr($text_color); ?>;"><?php echo esc_attr($sub_title); ?></h2>
+                                <h1 style="color: <?php echo esc_attr($text_color); ?>;"><?php echo esc_attr($title); ?></h1>
+                                <a class="button" href="<?php echo esc_url($button_link); ?>"><?php echo esc_attr($button_text); ?></a>
+                            </div>
+                       </div>
+                   </div>
+               </div>
+            </div>
 
-		<?php endwhile; ?>
+    <?php endwhile; ?>
 
     </div>
 </section>
@@ -134,88 +134,92 @@ get_header();
             <div class="tab-pane fade show active" id="brake" role="tabpanel">
                 <div class="product_carousel product_column5 owl-carousel">
                     <?php
-					  if(!function_exists('wc_get_products')) {
-					    return;
-					  }
-					  $paged                   = (get_query_var('paged')) ? absint(get_query_var('paged')) : 1;
-					 //$ordering                = WC()->query->get_catalog_ordering_args();
-					  //$ordering['orderby']     = array_shift(explode(' ', $ordering['orderby']));
-					  //$ordering['orderby']     = stristr($ordering['orderby'], 'price') ? 'meta_value_num' : $ordering['orderby'];
-					  $products_per_page       = 10;
-					  $featured_products       = wc_get_products(array(
-					    'meta_key'             => '_price',
-					    'status'               => 'publish',
-					    'limit'                => $products_per_page,
-					    'page'                 => $paged,
-					    'featured'             => true,
-					    'paginate'             => true,
-					    'return'               => 'ids',
-					  ));
-					  wc_set_loop_prop('current_page', $paged);
-					  wc_set_loop_prop('is_paginated', wc_string_to_bool(true));
-					  wc_set_loop_prop('page_template', get_page_template_slug());
-					  wc_set_loop_prop('per_page', $products_per_page);
-					  wc_set_loop_prop('total', $featured_products->total);
-					  wc_set_loop_prop('total_pages', $featured_products->max_num_pages);
+                    if(!function_exists('wc_get_products')) {
+                        return;
+                    }
+                    $paged                   = (get_query_var('paged')) ? absint(get_query_var('paged')) : 1;
+                    //$ordering                = WC()->query->get_catalog_ordering_args();
+                    //$ordering['orderby']     = array_shift(explode(' ', $ordering['orderby']));
+                    //$ordering['orderby']     = stristr($ordering['orderby'], 'price') ? 'meta_value_num' : $ordering['orderby'];
+                    $products_per_page       = 10;
+                    $featured_products       = wc_get_products(
+                        array(
+                        'meta_key'             => '_price',
+                        'status'               => 'publish',
+                        'limit'                => $products_per_page,
+                        'page'                 => $paged,
+                        'featured'             => true,
+                        'paginate'             => true,
+                        'return'               => 'ids',
+                        )
+                    );
+                    wc_set_loop_prop('current_page', $paged);
+                    wc_set_loop_prop('is_paginated', wc_string_to_bool(true));
+                    wc_set_loop_prop('page_template', get_page_template_slug());
+                    wc_set_loop_prop('per_page', $products_per_page);
+                    wc_set_loop_prop('total', $featured_products->total);
+                    wc_set_loop_prop('total_pages', $featured_products->max_num_pages);
 
-					  if($featured_products) {
-					    //do_action('woocommerce_before_shop_loop');
-					    woocommerce_product_loop_start();
-					      foreach($featured_products->products as $featured_product) {
-					        $post_object = get_post($featured_product);
-					        setup_postdata($GLOBALS['post'] =& $post_object);
-					        wc_get_template_part('content', 'product-home');
-					      }
-					      wp_reset_postdata();
-					    woocommerce_product_loop_end();
-					    //do_action('woocommerce_after_shop_loop');
-					  } else {
-					    do_action('woocommerce_no_products_found');
-					  }
-					?>
+                    if($featured_products) {
+                        //do_action('woocommerce_before_shop_loop');
+                        woocommerce_product_loop_start();
+                        foreach($featured_products->products as $featured_product) {
+                            $post_object = get_post($featured_product);
+                            setup_postdata($GLOBALS['post'] =& $post_object);
+                            wc_get_template_part('content', 'product-home');
+                        }
+                        wp_reset_postdata();
+                        woocommerce_product_loop_end();
+                        //do_action('woocommerce_after_shop_loop');
+                    } else {
+                        do_action('woocommerce_no_products_found');
+                    }
+                    ?>
                 </div>
             </div>
             <div class="tab-pane fade" id="wheels" role="tabpanel">
                  <div class="product_carousel product_column5 owl-carousel">
                     <?php
-					  if(!function_exists('wc_get_products')) {
-					    return;
-					  }
-					  $paged                   = (get_query_var('paged')) ? absint(get_query_var('paged')) : 1;
-					  $products_per_page       = 10;
-					  $recent_products       = wc_get_products(array(
-					    'meta_key'             => '_price',
-					    'status'               => 'publish',
-					    'limit'                => $products_per_page,
-					    'page'                 => $paged,
-					    'featured'             => false,
-					    'paginate'             => true,
-					    'return'               => 'ids',
-					    'orderby'              => 'date',
-    					//'order'                => $ordering['order'],
-					  ));
-					  wc_set_loop_prop('current_page', $paged);
-					  wc_set_loop_prop('is_paginated', wc_string_to_bool(true));
-					  wc_set_loop_prop('page_template', get_page_template_slug());
-					  wc_set_loop_prop('per_page', $products_per_page);
-					  wc_set_loop_prop('total', $recent_products->total);
-					  wc_set_loop_prop('total_pages', $recent_products->max_num_pages);
+                    if(!function_exists('wc_get_products')) {
+                        return;
+                    }
+                    $paged                   = (get_query_var('paged')) ? absint(get_query_var('paged')) : 1;
+                    $products_per_page       = 10;
+                    $recent_products       = wc_get_products(
+                        array(
+                        'meta_key'             => '_price',
+                        'status'               => 'publish',
+                        'limit'                => $products_per_page,
+                        'page'                 => $paged,
+                        'featured'             => false,
+                        'paginate'             => true,
+                        'return'               => 'ids',
+                        'orderby'              => 'date',
+                        //'order'                => $ordering['order'],
+                        )
+                    );
+                    wc_set_loop_prop('current_page', $paged);
+                    wc_set_loop_prop('is_paginated', wc_string_to_bool(true));
+                    wc_set_loop_prop('page_template', get_page_template_slug());
+                    wc_set_loop_prop('per_page', $products_per_page);
+                    wc_set_loop_prop('total', $recent_products->total);
+                    wc_set_loop_prop('total_pages', $recent_products->max_num_pages);
 
-					  if($recent_products) {
-					    //do_action('woocommerce_before_shop_loop');
-					    woocommerce_product_loop_start();
-					      foreach($recent_products->products as $recent_product) {
-					        $post_object = get_post($recent_product);
-					        setup_postdata($GLOBALS['post'] =& $post_object);
-					        wc_get_template_part('content', 'product-home');
-					      }
-					      wp_reset_postdata();
-					    woocommerce_product_loop_end();
-					    //do_action('woocommerce_after_shop_loop');
-					  } else {
-					    do_action('woocommerce_no_products_found');
-					  }
-					?>
+                    if($recent_products) {
+                        //do_action('woocommerce_before_shop_loop');
+                        woocommerce_product_loop_start();
+                        foreach($recent_products->products as $recent_product) {
+                            $post_object = get_post($recent_product);
+                            setup_postdata($GLOBALS['post'] =& $post_object);
+                            wc_get_template_part('content', 'product-home');
+                        }
+                        wp_reset_postdata();
+                        woocommerce_product_loop_end();
+                        //do_action('woocommerce_after_shop_loop');
+                    } else {
+                        do_action('woocommerce_no_products_found');
+                    }
+                    ?>
                 </div>
             </div>
         </div>
