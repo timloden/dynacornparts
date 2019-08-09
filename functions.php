b9cbfef358309c9981c3613b3df0bf19e762a419
Tim Loden <tim.m.loden@gmail.com>
2019-08-09 14:10:16 -0700

    added plugin update checker

--- functions.php
+++ functions.php
@@ -7,81 +7,100 @@
  * @package Dynacorn_Parts
  */
 
-if ( ! function_exists( 'dynacornparts_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
-	function dynacornparts_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on Dynacorn Parts, use a find and replace
-		 * to change 'dynacornparts' to the name of your theme in all the template files.
-		 */
-		load_theme_textdomain( 'dynacornparts', get_template_directory() . '/languages' );
+require 'plugin-update-checker/plugin-update-checker.php';
 
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+    'https://github.com/timloden/dynacornparts',
+    __FILE__,
+    'dynacornparts'
+);
 
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
+if (! function_exists('dynacornparts_setup') ) :
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * Note that this function is hooked into the after_setup_theme hook, which
+     * runs before the init hook. The init hook is too late for some features, such
+     * as indicating support for post thumbnails.
+     */
+    function dynacornparts_setup()
+    {
+        /*
+        * Make theme available for translation.
+        * Translations can be filed in the /languages/ directory.
+        * If you're building a theme based on Dynacorn Parts, use a find and replace
+        * to change 'dynacornparts' to the name of your theme in all the template files.
+        */
+        load_theme_textdomain('dynacornparts', get_template_directory() . '/languages');
 
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
+        // Add default posts and comments RSS feed links to head.
+        add_theme_support('automatic-feed-links');
 
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'dynacornparts' ),
-		) );
+        /*
+        * Let WordPress manage the document title.
+        * By adding theme support, we declare that this theme does not use a
+        * hard-coded <title> tag in the document head, and expect WordPress to
+        * provide it for us.
+        */
+        add_theme_support('title-tag');
 
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support( 'html5', array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-		) );
+        /*
+        * Enable support for Post Thumbnails on posts and pages.
+        *
+        * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+        */
+        add_theme_support('post-thumbnails');
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'dynacornparts_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
-		) ) );
+        // This theme uses wp_nav_menu() in one location.
+        register_nav_menus(
+            array(
+            'menu-1' => esc_html__('Primary', 'dynacornparts'),
+            ) 
+        );
 
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
+        /*
+        * Switch default core markup for search form, comment form, and comments
+        * to output valid HTML5.
+        */
+        add_theme_support(
+            'html5', array(
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+            ) 
+        );
 
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support( 'custom-logo', array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		) );
-	}
+        // Set up the WordPress core custom background feature.
+        add_theme_support(
+            'custom-background', apply_filters(
+                'dynacornparts_custom_background_args', array(
+                'default-color' => 'ffffff',
+                'default-image' => '',
+                ) 
+            ) 
+        );
+
+        // Add theme support for selective refresh for widgets.
+        add_theme_support('customize-selective-refresh-widgets');
+
+        /**
+         * Add support for core custom logo.
+         *
+         * @link https://codex.wordpress.org/Theme_Logo
+         */
+        add_theme_support(
+            'custom-logo', array(
+            'height'      => 250,
+            'width'       => 250,
+            'flex-width'  => true,
+            'flex-height' => true,
+            ) 
+        );
+    }
 endif;
-add_action( 'after_setup_theme', 'dynacornparts_setup' );
+add_action('after_setup_theme', 'dynacornparts_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -90,54 +109,60 @@ add_action( 'after_setup_theme', 'dynacornparts_setup' );
  *
  * @global int $content_width
  */
-function dynacornparts_content_width() {
-	// This variable is intended to be overruled from themes.
-	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
+function dynacornparts_content_width()
+{
+    // This variable is intended to be overruled from themes.
+    // Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
 	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'dynacornparts_content_width', 640 );
+    $GLOBALS['content_width'] = apply_filters('dynacornparts_content_width', 640);
 }
-add_action( 'after_setup_theme', 'dynacornparts_content_width', 0 );
+add_action('after_setup_theme', 'dynacornparts_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function dynacornparts_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'dynacornparts' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'dynacornparts' ),
-		'before_widget' => '<section id="%1$s" class="widget widget_list %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
+function dynacornparts_widgets_init()
+{
+    register_sidebar(
+        array(
+        'name'          => esc_html__('Sidebar', 'dynacornparts'),
+        'id'            => 'sidebar-1',
+        'description'   => esc_html__('Add widgets here.', 'dynacornparts'),
+        'before_widget' => '<section id="%1$s" class="widget widget_list %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+        ) 
+    );
 }
-add_action( 'widgets_init', 'dynacornparts_widgets_init' );
+add_action('widgets_init', 'dynacornparts_widgets_init');
 
 
 
 /* Autoload function files
 --------------------------------------------------------------------------------------*/
 
-add_action( 'after_setup_theme', 'caweb_autoload' );
+add_action('after_setup_theme', 'caweb_autoload');
 
-function caweb_autoload() {
-	$function_path = pathinfo( __FILE__ );
+function caweb_autoload()
+{
+    $function_path = pathinfo(__FILE__);
 
-	foreach ( glob( $function_path['dirname'] . '/inc/*.php' ) as $file ) {
-		require_once $file;
-	}
+    foreach ( glob($function_path['dirname'] . '/inc/*.php') as $file ) {
+        include_once $file;
+    }
 }
 
-function dynacornparts_woocommerce_setup() {
-	add_theme_support( 'woocommerce' );
-	add_theme_support( 'wc-product-gallery-zoom' );
-	add_theme_support( 'wc-product-gallery-lightbox' );
-	add_theme_support( 'wc-product-gallery-slider' );
+function dynacornparts_woocommerce_setup()
+{
+    add_theme_support('woocommerce');
+    add_theme_support('wc-product-gallery-zoom');
+    add_theme_support('wc-product-gallery-lightbox');
+    add_theme_support('wc-product-gallery-slider');
 }
-add_action( 'after_setup_theme', 'dynacornparts_woocommerce_setup' );
+add_action('after_setup_theme', 'dynacornparts_woocommerce_setup');
 
 /**
  * Disable the default WooCommerce stylesheet.
@@ -147,4 +172,4 @@ add_action( 'after_setup_theme', 'dynacornparts_woocommerce_setup' );
  *
  * @link https://docs.woocommerce.com/document/disable-the-default-stylesheet/
  */
-add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+add_filter('woocommerce_enqueue_styles', '__return_empty_array');
