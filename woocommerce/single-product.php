ffa85814910bd6f685bfa83c3e1065017f406d9b
Tim Loden <tim.m.loden@gmail.com>
2019-08-22 13:27:30 -0700

    moved cta to its own template file

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -55,34 +55,7 @@ get_header( 'shop' ); ?>
 		//do_action( 'woocommerce_sidebar' );
 	?>
 
-<!--call to action start-->
-<section class="call_to_action">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="call_action_inner">
-                    <div class="call_text">
-                        <h3>We Have <span>Recommendations</span>  for You</h3>
-                        <p>Take 30% off when you spend $150 or more with code Autima11</p>
-                    </div>
-                    <div class="discover_now">
-                        <a href="#">discover now</a>
-                    </div>
-                    <div class="link_follow">
-                        <ul>
-                            <li><a href="#"><i class="ion-social-facebook"></i></a></li>
-                            <li><a href="#"><i class="ion-social-twitter"></i></a></li>
-                            <li><a href="#"><i class="ion-social-googleplus"></i></a></li>
-                            <li><a href="#"><i class="ion-social-youtube"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<!--call to action end-->
-
+<?php get_template_part('template-parts/cta', 'global'); ?>
 
 <?php get_footer( 'shop' );
 
