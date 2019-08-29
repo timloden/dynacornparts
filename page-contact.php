e6667b9e8969a52074c095d7cd38b92dbb1659e6
Tim Loden <tim.m.loden@gmail.com>
2019-08-29 16:11:42 -0700

    added contact form settings

--- page-contact.php
+++ page-contact.php
@@ -13,6 +13,10 @@
  */
 
 get_header();
+$sub_title = get_field('description');
+$email = get_field('email_address');
+$phone = get_field('phone_number');
+$form_id = get_field('contact_form_id');
 ?>
 
  <div class="contact_area">
@@ -20,18 +24,28 @@ get_header();
         <div class="row">
             <div class="col-lg-6 col-md-12">
                <div class="contact_message content">
-                    <h3>contact us</h3>
-                     <p>Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram anteposuerit litterarum formas human. qui sequitur mutationem consuetudium lectorum. Mirum est notare quam</p>
+                    <h3>Contact us</h3>
+                    <?php if ($sub_title) : ?>
+                        <p><?php echo esc_attr($sub_title); ?></p>
+                    <?php endif; ?>
                     <ul>
-                        <li><i class="fa fa-phone"></i> <a href="#">info@dynacornparts.com</a></li>
-                        <li><i class="fa fa-envelope-o"></i>(916) 770-5565</li>
+                    <?php if ($email) : ?>
+                        <li><i class="fa fa-envelope-o"></i> <a href="mailto:<?php echo esc_attr($email); ?>"><?php echo esc_attr($email); ?></a></li>
+                        <?php endif; ?>
+                    <?php if ($phone) : ?>
+                        <li><i class="fa fa-phone"></i><?php echo esc_attr($phone); ?></li>
+                    <?php endif; ?>
                     </ul>
                 </div>
             </div>
             <div class="col-lg-6 col-md-12">
                <div class="contact_message form">
                     <h3>Send us a message</h3>
-                    <?php gravity_form(1, false, false, false, '', true, 12); ?>
+                    <?php
+                        if ($form_id) {
+                            gravity_form($form_id, false, false, false, '', true, 12); 
+                        }
+                    ?>
                 </div>
             </div>
         </div>
@@ -39,8 +53,6 @@ get_header();
 </div>
 
 <!--contact area end-->
-
-
 
 <?php
 
